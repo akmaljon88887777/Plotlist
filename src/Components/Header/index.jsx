7f8c08acde2d169d8d32414bb090db6ddc9484f0
Akmaljon <example@gmail.com>
2024-03-27 22:32:31 +0500

    to'g'irlandi

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -17,51 +17,51 @@ function Header() {
           </h4>
         </div>
         <br />
-        <div className="flex  justify-center">
-          <div className="w-[85%] lg:h-[70px] h-[400px] rounded bg-[#fff] absolute lg:grid lg:grid-cols-4 grid-cols-1 md:grid grid lg:flex-row justify-items-center items-center">
-            <div className="border-r border-gray-400 px-5">
-              <select
-                className="xl:w-[298px] lg:w-[290px] md:w-[280px] w-[200px] h-[36px] outline-none"
-                name=""
-                id=""
+        <div>
+          <div className="flex justify-center">
+            <div className="w-[84%] lg:h-[70px] h-[400px] rounded bg-[#fff] absolute lg:grid lg:grid-cols-4 grid-cols-1 md:grid grid lg:flex-row justify-items-center items-center">
+              <div className="border-r border-gray-400 px-5">
+                <select
+                  className="xl:w-[298px] lg:w-[290px] md:w-[280px] w-[200px] h-[36px] outline-none"
+                  name=""
+                  id=""
+                >
+                  <option value="">All Areas</option>
+                  <option value="">New Village</option>
+                  <option value="">Old Town</option>
+                  <option value="">Modern City</option>
+                </select>
+              </div>
+              <div className="px-6 border-r border-gray-400">
+                <input
+                  type="address"
+                  name="address"
+                  placeholder="Enter a location"
+                  autocomplete="on"
+                  className="xl:w-[298px] lg:w-[290px] md:w-[280px] w-[200px] h-[36px] outline-none"
+                />
+              </div>
+              <div className="px-6">
+                <select
+                  name="price"
+                  className="xl:w-[298px] lg:w-[290px] md:w-[280px] w-[200px] h-[36px] outline-none"
+                  aria-label="Default select example"
+                >
+                  <option>Price Range</option>
+                  <option value="$100 - $250">$100 - $250</option>
+                  <option value="$250 - $500">$250 - $500</option>
+                  <option value="$500 - $1000">$500 - $1,000</option>
+                  <option value="$1000+">$1,000 or more</option>
+                </select>
+              </div>
+              <button
+                className="xl:w-[298px] lg:w-[290px] md:w-[280px] w-[200px] h-[70px] bg-[#8d99af] flex justify-center items-center gap-5 text-white rounded-e"
+                type="submit"
               >
-                <option value="">All Areas</option>
-                <option value="">New Village</option>
-                <option value="">Old Town</option>
-                <option value="">Modern City</option>
-              </select>
+                <i className="fa fa-search w-10 h-10 rounded-full bg-[#FFFFFF] text-[#8d99af] flex items-center justify-center"></i>
+                Search Now
+              </button>
             </div>
-            <div className="px-6 border-r border-gray-400">
-              <input
-                type="address"
-                name="address"
-                placeholder="Enter a location"
-                autocomplete="on"
-                required=""
-                className="xl:w-[298px] lg:w-[290px] md:w-[280px] w-[200px] h-[36px] outline-none"
-              />
-            </div>
-            <div className="px-6">
-              <select
-                name="price"
-                className="xl:w-[298px] lg:w-[290px] md:w-[280px] w-[200px] h-[36px] outline-none"
-                aria-label="Default select example"
-                onchange="this.form.click()"
-              >
-                <option selected="">Price Range</option>
-                <option value="$100 - $250">$100 - $250</option>
-                <option value="$250 - $500">$250 - $500</option>
-                <option value="$500 - $1000">$500 - $1,000</option>
-                <option value="$1000+">$1,000 or more</option>
-              </select>
-            </div>
-            <button
-              className="xl:w-[298px] lg:w-[290px] md:w-[280px] w-[200px] h-[70px] bg-[#8d99af] flex justify-center items-center gap-5 text-white rounded-e"
-              type="submit"
-            >
-              <i class="fa fa-search w-10 h-10 rounded-full bg-[#FFFFFF] text-[#8d99af] flex items-center justify-center"></i>
-              Search Now
-            </button>
           </div>
         </div>
       </div>
